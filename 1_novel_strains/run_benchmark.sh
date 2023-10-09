5a4a6ed458d2c96e7ee27d5a41bd72654869a0ed
Ben Woodcroft <benjwoodcroft@gmail.com>
2023-10-09 10:20:42 +1000

    restrict running of benchmark 1 to 1 thread in sh.

--- 1_novel_strains/run_benchmark.sh
+++ 1_novel_strains/run_benchmark.sh
@@ -2,4 +2,5 @@
 
 rm -rf benchmarks
 
-for i in `echo 32 8 1`; do rm -rf output* && snakemake -c 32 --use-conda --conda-frontend mamba --configfile config-${i}threads.yaml; done
+# for i in `echo 32 8 1`; do rm -rf output* && snakemake -c 32 --use-conda --conda-frontend mamba --configfile config-${i}threads.yaml; done
+for i in `echo 1`; do rm -rf output* && snakemake -c 32 --use-conda --conda-frontend mamba --configfile config-${i}threads.yaml; done
